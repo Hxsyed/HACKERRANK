b4a1855a68141105f7bee4eee98ff6ade5f443df
Hxsyed <syedharoon@outlook.com>
2020-04-05 22:41:06 -0400

    Input and Output

---
+++ InputOutput/IO.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int a,b,c,d; // declares variables a,b,c,d.
+    cin >> a >> b>> c; // takes in input form user 
+    d = a+b+c; // sums all 3 integers 
+    cout << d; // outputs the sum of all 3 integers.
+    return 0;
+}
