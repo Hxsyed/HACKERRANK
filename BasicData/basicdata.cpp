e91ff8e879f0c820351860d81fa15d9ce0631227
Hxsyed <syedharoon@outlook.com>
2020-04-05 22:57:45 -0400

    Basic Data

---
+++ BasicData/basicdata.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a; // declaring integer a
+    long b; // declaring long b
+    char c; // declaring char c
+    float d; // declaring float d
+    double e; // declaring double e
+    cin >> a >> b >> c >> d >> e; // input
+    cout << a << endl; // output int 
+    cout << b << endl; // output long	
+    cout << c << endl; // output char
+    cout << fixed << setprecision(3) << d << endl; // outputs float with 3 decimal point
+    cout << fixed <<setprecision(9) << e << endl; // outputs double with 9 decimal point
+    return 0;
+}
